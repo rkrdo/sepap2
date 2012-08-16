f44a26be52cb2dfc0b9f9a36d41996140b101ef1
Roberto Plancarteclear <robertoplancarte@gmail.com>
2012-08-15 21:37:21 -0500

    first commit

---
+++ sepap/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sepap::Application.config.secret_token = '54d803b01034c2e7978ed02e87397a32be7357da27d78d809f7419a849983d27a6421cb1c657e0450d9fc5163c20b4c6f1d4535492882fd7e59fd40c44a5bde4'
