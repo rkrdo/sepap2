1f73d228c50529d473f0c592994f016816646ddb
Roberto Plancarteclear <robertoplancarte@gmail.com>
2012-08-23 18:45:13 -0500

    added jquery

---
+++ app/assets/javascripts/problem.js
@@ -0,0 +1,12 @@
+$(function() {
+  $( "#problem_info" ).tabs();
+  $( ".tabs-bottom .ui-tabs-nav, .tabs-bottom .ui-tabs-nav > *" )
+    .removeClass( "ui-corner-all ui-corner-top" )
+    .addClass( "ui-corner-bottom" );
+});
+$(function() {
+  $( "#problem_attempt" ).tabs();
+  $( ".tabs-bottom .ui-tabs-nav, .tabs-bottom .ui-tabs-nav > *" )
+    .removeClass( "ui-corner-all ui-corner-top" )
+    .addClass( "ui-corner-bottom" );
+});
