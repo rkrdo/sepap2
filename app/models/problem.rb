c329cd2b209f0f3d4134b78c4f8105aa25d3c623
Guayo <guayo@Batman.(none)>
2012-08-30 21:00:41 -0500

    Validate MainUploader

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -7,9 +7,7 @@ class Problem < ActiveRecord::Base
 
 
   validates :title, :description, :main, :presence => true
-
-  validates :method, :presence => true
-
   validates_numericality_of :time, :greater_than_or_equal_to =>1, :message => "El tiempo no puede ser negativo."
+
 
 end
