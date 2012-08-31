dd976b03135593018406dd45bb5b7a0927184169
Guayo <guayo@Batman.(none)>
2012-08-30 20:27:25 -0500

    Some problem validations

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -5,4 +5,11 @@ class Problem < ActiveRecord::Base
   mount_uploader :main, MainUploader
   mount_uploader :method, MethodUploader
 
+
+  validates :title, :description, :main, :presence => true
+
+  validates :method, :presence => true
+
+  validates_numericality_of :time, :greater_than_or_equal_to =>1, :message => "El tiempo no puede ser negativo."
+
 end
