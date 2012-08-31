2ba159ef5fd1521f22e5e4fbdb6a2265e0f23f96
Guayo <guayo@Batman.(none)>
2012-08-30 19:26:07 -0500

    Method Uploader

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,7 +1,8 @@
 class Problem < ActiveRecord::Base
   has_many :attempts, :dependent => :delete_all
-  attr_accessible :description, :module, :time, :title, :main
+  attr_accessible :description, :module, :time, :title, :main, :method
 
   mount_uploader :main, MainUploader
+  mount_uploader :method, MethodUploader
 
 end
