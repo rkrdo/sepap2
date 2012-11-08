bbeefe5e566b0ed06e5a0af7637eb2215bfdffdb
Roberto Plancarte <robertoplancarte@gmail.com>
2012-11-08 17:19:35 -0600

    fixes routing bug

--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,7 @@ end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  match '/:locale' => 'home#index'
   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
