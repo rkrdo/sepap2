b2fcd15602869f847c7eff9d641efa7f6bde37b0
Miguel Cervera <miguelcerverac@gmail.com>
2013-03-04 13:01:28 -0600

    remove active_admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130301195325) do
+ActiveRecord::Schema.define(:version => 20130304183805) do
   create_table "assignments", :force => true do |t|
     t.string   "title"
     t.integer  "problem_id"
