e9aa69de35635d223b1b48bbe1ab0654be59e13c
Miguel Cervera <miguelcerverac@gmail.com>
2013-03-07 12:55:34 -0600

    fixed the routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < ActionController::Base
   
   def set_locale
   	I18n.locale = params[:locale] || I18n.default_locale
+  	params[:locale] = I18n.locale
   end
 
  def default_url_options(options={})
