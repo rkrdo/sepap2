6fa621e50dec1a4a30007fe5ad863157c9e0d90b
Miguel Cervera <miguelcerverac@gmail.com>
2013-04-24 00:15:32 -0500

    fixed the case statement with strings

--- app/helpers/attempts_helper.rb
+++ app/helpers/attempts_helper.rb
@@ -1,18 +1,18 @@
 module AttemptsHelper
   def class_for_status(status)
-    case status
-    when "accept"
-      return "icon-ok-sign icon-2x icono-accept"
-    when "timeout"
-      return "icon-time icon-2x icono-timeout"
-    when "fail"
-      return "icon-remove-sign icon-2x icono-fail"
-    when "compile_error"
-      return "icon-exclamation-sign icon-2x icono-uncompile"
-    when "compiling"
-      return "icon-cogs icon-2x icono-compiling"
-    else
-      return ""
+    case 
+      when status == "accept"
+        return "icon-ok-sign icon-2x icono-accept"
+      when status == "timeout"
+        return "icon-time icon-2x icono-timeout"
+      when status == "fail"
+        return "icon-remove-sign icon-2x icono-fail"
+      when status == "compile_error"
+        return "icon-exclamation-sign icon-2x icono-uncompile"
+      when status == "compiling"
+        return "icon-cogs icon-2x icono-compiling"
+      else
+        return ""
     end
   end
 
