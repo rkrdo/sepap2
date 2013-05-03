14a8779a21580c43d95572204cebd22084f802d2
Miguel Cervera <miguelcerverac@gmail.com>
2013-05-02 22:09:32 -0500

    delete assignments on problem deletion

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,8 +1,8 @@
 class Assignment < ActiveRecord::Base
   belongs_to :problem
   belongs_to :group
-  has_many  :attempts
-  has_many :users, through: :group
+  has_many  :attempts, :dependent => :nullify
+  has_many :users, :hrough => :group
   attr_accessible :due_date, :title, :problem_id, :group_id
   
   def accepted_for_user?(user)
